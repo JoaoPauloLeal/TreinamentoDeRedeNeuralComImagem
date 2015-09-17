8c696cb11308c7dbaf077952c032c1f5663955c4
João Paulo Leal <joaopaulolea@ulbra.edu.br>
2015-09-17 18:18:15 -0300

    RNA
    Rede Neural treinada com 2 padrões de imagens

---
+++ IAg1/Controllers/ProcessController.cs
@@ -0,0 +1,121 @@
+﻿using IAg1.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IAg1.Controllers
+{
+    public class ProcessController : Controller
+    {
+        public static string NomeImagem;
+        public ActionResult Upload()
+        {
+            Binarios.FotoArray();
+            Binarios.ExecutarAprendizado();
+            List<float> blist = new List<float>();
+            List<float> pesos = new List<float>();
+
+            return View();
+        }
+        public ActionResult Checar()
+        {
+            #region TestarImagem
+
+            List<float> blist = new List<float>();
+            List<float> pesos = new List<float>();
+            try
+            {
+            string path = Path.Combine(Server.MapPath("~/App_Data"), Path.GetFileName(NomeImagem));
+            Bitmap image = new Bitmap(path, true);
+            Bitmap novo = new Bitmap(image.Width, image.Height);
+
+            Console.WriteLine(image.PixelFormat);
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    /*Color clr = image.GetPixel(x, y);
+                    Color newclr = Color.FromArgb(0, clr.B, 0);
+                    image.SetPixel(x, y, newclr);*/
+                    Color originalColor = image.GetPixel(x, y);//pega a cor original
+                    int grayScale = (int)((originalColor.R * 0.3) + (originalColor.G * 0.59) + (originalColor.B * 0.11));
+                    if (grayScale < 110 && grayScale > -1)
+                    {
+                        blist.Add(0);
+                    }
+                    if (grayScale > 110 && grayScale < 256)
+                    {
+                        blist.Add(1);
+                    }
+                    Color CorEmEscalaDeCinza = Color.FromArgb(grayScale, grayScale, grayScale);
+                    novo.SetPixel(x, y, CorEmEscalaDeCinza);
+                    pesos.Add(0);
+                }
+            }
+            novo.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\temp\newn1.png");
+
+            #endregion
+            #region Teste-0e1-
+            float retorno = Binarios.Testar(blist);
+            if (retorno == 1)
+            {
+                ViewBag.Doenca = 1;
+            }
+            if (retorno == 0)
+            {
+                ViewBag.Doenca = 0;
+            }
+            #endregion
+            }
+            catch (Exception)
+            {
+
+                ViewBag.Doenca = -1;
+            }
+            return View();
+        }
+        [HttpPost]
+        public FilePathResult Image()
+        {
+            string filename = Request.Url.AbsolutePath.Replace("/Upload/image", "");
+            string contentType = "";
+            var filePath = new FileInfo(Server.MapPath("~/App_Data") + filename);
+            
+            var index = filename.LastIndexOf(".") + 1;
+            var extension = filename.Substring(index).ToUpperInvariant();
+
+            // Fix for IE not handling jpg image types
+            contentType = string.Compare(extension, "JPG") == 0 ? "image/jpeg" : string.Format("image/{0}", extension);
+
+            return File(filePath.FullName, contentType);
+        }
+        [HttpPost]
+        public ContentResult UploadFiles()
+        {
+            var r = new List<UploadFilesResult>();
+
+            foreach (string file in Request.Files)
+            {
+                HttpPostedFileBase hpf = Request.Files[file] as HttpPostedFileBase;
+                if (hpf.ContentLength == 0)
+                    continue;
+
+                string savedFileName = Path.Combine(Server.MapPath("~/App_Data"), Path.GetFileName(hpf.FileName));
+                hpf.SaveAs(savedFileName);
+
+                r.Add(new UploadFilesResult()
+                {
+                    Name = hpf.FileName,
+                    Length = hpf.ContentLength,
+                    Type = hpf.ContentType
+                });
+            }
+            NomeImagem = r[0].Name;
+            return Content("{\"name\":\"" + r[0].Name + "\",\"type\":\"" + r[0].Type + "\",\"size\":\"" + string.Format("{0} bytes", r[0].Length) + "\"}", "application/json");
+        }
+    }
+}
---
+++ IAg1/Models/Binarios.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace IAg1.Models
+{
+    public class Binarios
+    {
+        public static List<float> blistfinal1 = new List<float>();
+        public static List<float> blistfinal2 = new List<float>();
+        public static List<float> pesos = new List<float>();
+        
+        public static float v = 1;
+        public static float soma;
+        public static float w0 = 0;
+        
+
+        public static void FotoArray()
+        {
+            List<float> blist = new List<float>();
+            List<float> blist2 = new List<float>();
+
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\temp\n1.png";
+                Bitmap image = new Bitmap(path, true);
+                Bitmap novo = new Bitmap(image.Width, image.Height);
+            
+                for (int x = 0; x < image.Width; x++)
+                {
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Color originalColor = image.GetPixel(x, y);//pega a cor original
+                        int grayScale = (int)((originalColor.R * 0.3) + (originalColor.G * 0.59) + (originalColor.B * 0.11));
+                        if (grayScale <= 110 && grayScale > 0)
+                        {
+                            
+                            blist.Add(0);
+                        }
+                        if (grayScale >= 110 && grayScale < 256)
+                        {
+                            blist.Add(1);
+                        }
+                        Color CorEmEscalaDeCinza = Color.FromArgb(grayScale, grayScale, grayScale);
+                        novo.SetPixel(x, y, CorEmEscalaDeCinza);
+                    }
+                }
+
+            string path2 = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\temp\n2.png";
+            Bitmap image2 = new Bitmap(path2, true);
+            Bitmap novo2 = new Bitmap(image2.Width, image2.Height);
+
+            for (int x = 0; x < image2.Width; x++)
+            {
+                for (int y = 0; y < image2.Height; y++)
+                {
+                    Color originalColor = image2.GetPixel(x, y);//pega a cor original
+                    int grayScale = (int)((originalColor.R * 0.3) + (originalColor.G * 0.59) + (originalColor.B * 0.11));
+                    if (grayScale < 110 && grayScale > 0)
+                    {
+
+                        blist2.Add(0);
+                    }
+                    if (grayScale > 110 && grayScale < 256)
+                    {
+                        blist2.Add(1);
+                    }
+                    Color CorEmEscalaDeCinza = Color.FromArgb(grayScale, grayScale, grayScale);
+                    novo.SetPixel(x, y, CorEmEscalaDeCinza);
+                }
+            }
+
+
+            //novo.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\temp\newn1.png");
+            
+            
+                blistfinal1 = blist;
+            
+                blistfinal2 = blist2;
+            
+            for (int x = 0; x < 10000; x++)
+            {
+                pesos.Add(0);
+            }
+            
+        }
+        public static void ExecutarAprendizado()
+        {
+            float y1 = 1;
+            float y2 = 0;
+            float y = 0;
+            int cont = 0;
+            soma = 0;
+            //REGION TREINAMENTO
+            while (cont != 2)
+            {
+                
+                cont = 0;
+                //#region X=1
+                for (int x = 0; x < 10000; x++)
+                {
+                    soma += (blistfinal1[x] * pesos[x]);
+                }
+                soma += (v * w0);
+
+                if (soma <= 0)
+                {
+                    y = 0;
+                }
+                if (soma > 0)
+                {
+                    y = 1;
+                }
+                if (y != y1)
+                {
+
+                    for (int x = 0; x < 10000; x++)
+                    {
+                        pesos[x] = pesos[x] + 1 * (y1 - y) * blistfinal1[x];
+                    }
+                    w0 = w0 + 1 * (y1 - y) * v;
+                }
+                else
+                {
+                    cont++;
+                }
+                //#end region X=1
+                //#region X=2
+                for (int x = 0; x < 10000; x++)
+                {
+                    soma += (blistfinal2[x] * pesos[x]);
+                }
+                soma += (v * w0);
+
+                if (soma <= 0)
+                {
+                    y = 0;
+                }
+                if (soma > 0)
+                {
+                    y = 1;
+                }
+                if (y != y2)
+                {
+                    for (int x = 0; x < 10000; x++)
+                    {
+                        pesos[x] = pesos[x] + 1 * (y2 - y) * blistfinal2[x];
+                    }
+                    w0 = w0 + 1 * (y2 - y) * v;
+                }
+                else
+                {
+                    cont++;
+                }
+                //#end region X=2
+            }
+            //END REGION TREINAMENTO
+        }
+        public static List<float> FotoRetorna()
+        {
+            List<float> blist = new List<float>();
+            List<float> pesos = new List<float>();
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\temp\teste2.png";
+            Bitmap image = new Bitmap(path, true);
+            Bitmap novo = new Bitmap(image.Width, image.Height);
+
+            Console.WriteLine(image.PixelFormat);
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    /*Color clr = image.GetPixel(x, y);
+                    Color newclr = Color.FromArgb(0, clr.B, 0);
+                    image.SetPixel(x, y, newclr);*/
+                    Color originalColor = image.GetPixel(x, y);//pega a cor original
+                    int grayScale = (int)((originalColor.R * 0.3) + (originalColor.G * 0.59) + (originalColor.B * 0.11));
+                    if (grayScale < 110 && grayScale > -1)
+                    {
+                        blist.Add(0);
+                    }
+                    if (grayScale > 110 && grayScale < 256)
+                    {
+                        blist.Add(1);
+                    }
+                    Color CorEmEscalaDeCinza = Color.FromArgb(grayScale, grayScale, grayScale);
+                    novo.SetPixel(x, y, CorEmEscalaDeCinza);
+                    pesos.Add(0);
+                }
+            }
+            novo.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\temp\newn1.png");
+            return blist;
+        }
+        public static float Testar(List<float> ListaBinaria)
+        {
+            float y = 0;
+            float soma = 0;
+
+            for (int x = 0; x < 10000; x++)
+            {
+                soma += (ListaBinaria[x] * pesos[x]);
+            }
+            soma += (v * w0);
+
+            if (soma <= 0)
+            {
+                y = 0;
+            }
+            if (soma > 0)
+            {
+                y = 1;
+            }
+            return y;
+        }
+    }
+}
